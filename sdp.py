eaa504298ae04b1ff545f73d1e4ee3820497a097
akashi979 <65153860+akashi979@users.noreply.github.com>
2020-05-11 13:04:37 +0530

    Update sdp.py

--- sdp.py
+++ sdp.py
@@ -1,10 +1,10 @@
 n1 = int(input('Enter a number: '))
 n2 = int(input('Enter another number:'))
 if 1<=n1<=10**10 and 1<=n2<=10**10:
- sum = n1+n2
+ addition = n1+n2
  diff = n1-n2
  prod = n1*n2
- print (sum)                              
+ print (addition)                              
  print (diff)                       
  print (prod) 
   
